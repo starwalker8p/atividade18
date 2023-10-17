26634ec20e723e84e9f01be2038faca51486305e
starwalker8p <starwalker8p@gmail.com>
2023-10-17 10:45:53 -0300

    fiz a atividade

---
+++ maior---menor.py
@@ -0,0 +1,7 @@
+n1,n2=float(input("numero 1: ")), float(input("numero 2: "))
+if n1>n2:
+    print("o primeiro valor é maior")
+elif n2>n1:
+    print("o segundo valor é maior")
+else: 
+    print("não existe valor maior, os dois são iguais")
